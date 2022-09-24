fc66ab7edc73369df617b1001ad00324f4b02558
David Zheng <99851034+AsianOmelet@users.noreply.github.com>
2022-09-24 18:13:32 -0400

    Add files via upload
    Perfect Number calculator

--- NewClass.java
+++ NewClass.java
@@ -1,51 +1,41 @@
-
-import java.util.Scanner;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 /**
  *
  * @author 24zhendavi
  */
+import java.util.Scanner;
+
 public class NewClass {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        //this chunk asks for user input on height, bounciness, and bounces. Also declares the total
-        //variable for total distance traveled. 
-        double total = 0;
-        System.out.println("Initial Height ");
-        double height = in.nextDouble();
-        System.out.println("Amount of bounciness: .1 - .9");
-        double bounciness = in.nextDouble();
-        System.out.println("Number of bounces ");
-        int bounces = in.nextInt();
-        System.out.println("Down " + height);
-        //adds initial height to distance traveled
-        total += height;
-        //calculates the next height for the up distance
-        height = height * bounciness;
-        System.out.println("Up " + height);
-        //adds up distance
-        total += height;
-        //for loop so that the loop only interates as many times as bounces
-        for (int i = 0; i < bounces - 1; i++) {
-            //adds the height of the up bounce when it reads the loop again
-            total += height;
-            System.out.println("Down " + height);
-            //more logic to determine bounciness
-            height = height * bounciness;
-            total += height;
-            System.out.println("Up " + height);
-            //breaks loop so that if the number is incredibly small, the ball doesn't end up bouncing infinite
-            //times
-            if (height < .001) {
-                break;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your test number? ");
+        int num = input.nextInt();
+        int sum = divisorSum(num);
+        if (sum == num) {
+            System.out.println("You have a perfect number. ");
+        } else if (sum > num) {
+            System.out.println("You have an abundant number ");
+        } else {
+            System.out.println("You have a deficient number");
+        }
+    }
+
+    public static boolean isDivisor(int num, int Divisor) {
+        return num % Divisor == 0;
+    }
+
+    public static int divisorSum(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (isDivisor(number, i)) {
+                sum += i;
             }
         }
-        //prints out total distance traveled
-        System.out.println(total);
+        return sum;
     }
 }
